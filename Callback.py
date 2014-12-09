9f47aff8cc1d6633109adf5e5f7f3d0fa89e8419
Josh Dague <daguej@email.uc.edu>
2014-12-09 15:53:25 -0800

    Also allow a comma after the closing parenthesis

--- Callback.py
+++ Callback.py
@@ -5,7 +5,7 @@ class CallbackFunctionCommand(sublime_plugin.TextCommand):
 		view = self.view
 
 		for region in view.sel():
-			if view.substr(region.end()) == ')' and view.substr(region.end() + 1) != ';':
+			if view.substr(region.end()) == ')' and view.substr(region.end() + 1) != ';' and view.substr(region.end() + 1) != ',':
 				view.insert(edit, region.end() + 1, ';')
 
 		view.run_command("insert_snippet", { "contents": "function(err, ${1:d}) {\n\t${0:$SELECTION}\n}" })
