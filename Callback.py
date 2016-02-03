44852988a57f83a84684239425755273b5ed5fb7
Josh Dague <daguej@email.uc.edu>
2016-02-03 12:10:19 -0800

    Add named callback functions as alt+shift+c
    Closes #1

--- Callback.py
+++ Callback.py
@@ -4,8 +4,20 @@ class CallbackFunctionCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		view = self.view
 
-		for region in view.sel():
-			if view.substr(region.end()) == ')' and view.substr(region.end() + 1) != ';' and view.substr(region.end() + 1) != ',':
-				view.insert(edit, region.end() + 1, ';')
+		semicolon(edit, view)
 
 		view.run_command("insert_snippet", { "contents": "function(err, ${1:d}) {\n\t${0:$SELECTION}\n}" })
+
+class NamedCallbackFunctionCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+
+		semicolon(edit, view)
+
+		view.run_command("insert_snippet", { "contents": "function ${1}(err, ${2:d}) {\n\t${0:$SELECTION}\n}" })
+
+
+def semicolon(edit, view):
+	for region in view.sel():
+		if view.substr(region.end()) == ')' and view.substr(region.end() + 1) != ';' and view.substr(region.end() + 1) != ',':
+			view.insert(edit, region.end() + 1, ';')
